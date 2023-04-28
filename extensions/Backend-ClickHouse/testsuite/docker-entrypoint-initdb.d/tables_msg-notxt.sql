4df0d7c415c09320fe6999725dedb2a41908bed9
antonnixx <131792300+antonnixx@users.noreply.github.com>
2023-04-28 16:55:46 +0300

    Add files via upload
    SQL message table

---
+++ extensions/Backend-ClickHouse/testsuite/docker-entrypoint-initdb.d/tables_msg-notxt.sql
@@ -0,0 +1,16 @@
+--CREATE DATABASE IF NOT EXISTS uniset;--
+
+CREATE TABLE IF NOT EXISTS uniset.main_messages
+(
+    timestamp DateTime('UTC') DEFAULT now(),
+    time_usec UInt64 Codec(DoubleDelta, LZ4),
+    value Float64 Codec(DoubleDelta, LZ4),
+    name LowCardinality(String),
+    uniset_hid UInt32 Default murmurHash2_32(name),
+    name_hid UInt64 Default cityHash64(name),
+    msg_hid UInt32 Default murmurHash2_32(concat(name, toString(value)))
+) ENGINE = MergeTree
+PARTITION BY toStartOfDay(timestamp)
+PRIMARY KEY(timestamp,time_usec,name_hid)
+ORDER BY (timestamp,time_usec,name_hid)
+TTL timestamp + INTERVAL 90 DAY;
